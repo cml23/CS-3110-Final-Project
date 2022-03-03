089ca50125f1c14e73baf41b4426e31fb772e65c
Cassidy Langenfeld <cml297@cornell.edu>
2022-03-03 18:26:02 -0500

    Update Canvas.ml and Canvas.mli
    -Add documentation and stubs for functions
    -Updated dune to include Graphics library

---
+++ src/canvas.ml
@@ -0,0 +1,14 @@
+(**[draw_board brd st] draws the board to the game canvas using the
+   board configuration [bd].*)
+let draw_board : (*Board.t ->*) State.t -> unit =
+  failwith "Unimplemented: draw_board"
+
+(**[draw_piece p] draws the piece to the board, in the center of the x
+   and y coordinates of the appropriate tile specified in [p].*)
+let draw_piece : (*Board.piece -> *) unit =
+  failwith "Unimplemented: draw_piece"
+
+let draw_tile : (*Board.tile -> *) unit =
+  failwith "Unimplemented: draw_tile"
+
+let draw st = failwith "Unimplemented: draw"
