8b1b3e545aec681719994822d503be9863efcd55
Anirudh Sharma <as2844@cornell.edu>
2022-03-07 01:51:49 -0500

    Implement functions in Board

--- src/board.ml
+++ src/board.ml
@@ -1,31 +1,104 @@
-type piece =
-  | Some of {
-      player : string;
-      id : int;
-      is_royal : bool;
-    }
-  | None
-
-type tile = {
-  x : int;
-  (* maybe get rid of x,y and introduce functions to get x and y from
-     the tile array *)
-  y : int;
-  pc : piece;
+type piece = {
+  player : int;
+  id : int;
+  is_royal : bool;
 }
 
-type t = tile array * int
+type tile =
+  | Some of piece
+  | None
 
-let init_board : t = raise (Failure "Unimplemented: Board.init_board")
+type t = tile array * int
+(* a board can have any rectangular shape. The number of tiles per row
+   (i.e., the number of columns) is given by the second number in the
+   tuple representation. *)
+
+let std_pc p i = Some { player = p; id = i; is_royal = false }
+
+let init_board : t =
+  ( [|
+      std_pc 1 1;
+      None;
+      std_pc 1 2;
+      None;
+      std_pc 1 3;
+      None;
+      std_pc 1 4;
+      None;
+      None;
+      std_pc 1 5;
+      None;
+      std_pc 1 6;
+      None;
+      std_pc 1 7;
+      None;
+      std_pc 1 8;
+      std_pc 1 9;
+      None;
+      std_pc 1 10;
+      None;
+      std_pc 1 11;
+      None;
+      std_pc 1 12;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      None;
+      std_pc 2 13;
+      None;
+      std_pc 2 14;
+      None;
+      std_pc 2 15;
+      None;
+      std_pc 2 16;
+      std_pc 2 17;
+      None;
+      std_pc 2 18;
+      None;
+      std_pc 2 19;
+      None;
+      std_pc 2 20;
+      None;
+      None;
+      std_pc 2 21;
+      None;
+      std_pc 2 22;
+      None;
+      std_pc 2 23;
+      None;
+      std_pc 2 24;
+    |],
+    8 )
+
 let from_json json = raise (Failure "Unimplemented: Board.from_json")
+let get_x (b : t) i = ((i - 1) mod snd b) + 1
+let get_y (b : t) i = ((i - 1) / snd b) + 1
+let get_idx (b : t) x y = (snd b * (y - 1)) + x
+let dim_x (b : t) = snd b
+let dim_y (b : t) = Array.length (fst b) / snd b
+
+let piece_of_xy (b : t) x y =
+  match Array.get (fst b) (get_idx b x y - 1) with
+  | None -> (None : piece option)
+  | Some pc -> (Some pc : piece option)
 
 let pieces_of_player (board : t) (pl : string) : string list =
   raise (Failure "Unimplemented: Board.pieces_of_player")
-(* function on a tile that returns None if no piece or Some of {player,
-   id, is_royal} if a tile has a piece; function for the neighbors of
-   tile; individual functions for getting each of 4 neighbors: return
-   None if nothing there, or Some of (x,y) *)
 
-(* (priority) for drawing: function for returning dimensions of board;
-   (priority) function to provide list of tile info for every tile in
-   board *)
+(* TODO: functions for the neighbors of tile; individual functions for
+   getting each of 4 neighbors: return None if nothing there, or Some of
+   (x,y) *)
--- src/board.mli
+++ src/board.mli
@@ -1,13 +1,46 @@
+type piece = {
+  player : int;
+  id : int;
+  is_royal : bool;
+}
+(** The type representing pieces. *)
+
 type t
 (** The abstract type of values representing boards. *)
 
 val init_board : t
-(** [init_board] is the abstract representation of the default board at
-    the start of the game. *)
+(** [init_board] is the abstract representation of the default 8x8 board
+    at the start of the game. *)
 
 val from_json : Yojson.Basic.t -> t
 (** [from_json j] is the board that [j] represents. Requires: [j] is a
     valid JSON board representation. *)
+
+val get_x : t -> int -> int
+(** [get_x b i] is the x coordinate on board [b] corresponding to the
+    general index [i], which is 1 at the bottom left and maximum at the
+    top right. For example, on a standard 8x8 board, the x coordinate
+    for index 24 is 8. *)
+
+val get_y : t -> int -> int
+(** [get_y b i] is the y coordinate on board [b] corresponding to the
+    general index [i], which is 1 at the bottom left and maximum at the
+    top right. For example, on a standard 8x8 board, the y coordinate
+    for index 24 is 3. *)
+
+val get_idx : t -> int -> int -> int
+(** [get_idx b x y] is the general index corresponding to coordinates x
+    and y on board b. *)
+
+val dim_x : t -> int
+(** [dim_x b] is the x-dimension of board b. *)
+
+val dim_y : t -> int
+(** [dim_y b] is the y-dimension of board b. *)
+
+val piece_of_xy : t -> int -> int -> piece option
+(** [piece_of_idx b x y] is Some the piece located at position (x,y) on
+    board b if a piece exists at that position; otherwise it is None. *)
 
 val pieces_of_player : t -> string -> string list
 (** [pieces_of_player b pl] is the list of pieces on board [b] belonging
