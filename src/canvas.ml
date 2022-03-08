efae86da88eefac0161171205840ab93ddaac051
Cassidy Langenfeld <cml297@cornell.edu>
2022-03-07 23:46:17 -0500

    Implement Canvas.draw_piece
    -Implementation based on recent changes to Board.ml
    -Implementation of other functions in Canvas need to be updated to use
    Board
    -Small changes were also made to Canvas.draw
    -Constants for tile_size and piece size also added

--- src/canvas.ml
+++ src/canvas.ml
@@ -1,6 +1,16 @@
-(**[draw_piece tile_id] draws a piece to the specified tile.*)
-let draw_piece (tile_id : int) : unit =
-  failwith "Unimplemented: draw_piece"
+(**[tile_size] is a constant representing the x and y dimensions of a
+   tile. *)
+let tile_size = 40
+
+(**[pc_size] is a constant representing the horizontal and vertical
+   radius of a piece. *)
+let pc_size = 40
+
+(**[draw_piece pc x y] draws [pc] to the tile at [x] and [y].*)
+let draw_piece (pc : Board.piece) (x : int) (y : int) : unit =
+  let open Graphics in
+  if pc.player = 1 then set_color red else set_color blue;
+  fill_ellipse x y 40 40
 
 (**[draw_tile x y tile_size color] draws a tile of [color] and size
    [tile_size] at position [(x,y)]*)
@@ -35,8 +45,12 @@ let rec draw_board
     (tile_size : int) : unit =
   draw_row x y tile_size color cols;
   if rows = 0 then ()
-  else draw_board x (y + tile_size) color (rows - 1) cols tile_size
+  else draw_board x (y - tile_size) color (rows - 1) cols tile_size
 
 let draw st =
-  Graphics.open_graph "";
-  draw_board 0 0 8 8 Graphics.black 40
+  let open Graphics in
+  open_graph "";
+  (*draw_board 0 (size_y () - (tile_size - 10)) 8 8 black 40;*)
+  draw_piece { player = 2; id = 1; is_royal = false } 50 50;
+  synchronize ();
+  ignore (wait_next_event [ Key_pressed ])
