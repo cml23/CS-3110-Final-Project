23b991ab34c85390f76a93d2323254a13625aeaf
Anirudh Sharma <as2844@cornell.edu>
2022-03-08 18:31:57 -0500

    Commented out unimplemented functions

--- src/board.mli
+++ src/board.mli
@@ -12,7 +12,7 @@ val init_board : t
 (** [init_board] is the abstract representation of the default 8x8 board
     at the start of the game. *)
 
-val from_json : Yojson.Basic.t -> t
+(*val from_json : Yojson.Basic.t -> t*)
 (** [from_json j] is the board that [j] represents. Requires: [j] is a
     valid JSON board representation. *)
 
@@ -42,7 +42,7 @@ val piece_of_xy : t -> int -> int -> piece option
 (** [piece_of_idx b x y] is Some the piece located at position (x,y) on
     board b if a piece exists at that position; otherwise it is None. *)
 
-val pieces_of_player : t -> string -> string list
+(*val pieces_of_player : t -> string -> string list*)
 (** [pieces_of_player b pl] is the list of pieces on board [b] belonging
     to player [pl]. *)
 
