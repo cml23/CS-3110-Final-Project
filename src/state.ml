dff474d39e42f0e42bbe9f6c197e8d7ddcdf37f5
hhf28 <hhf28@cornell.edu>
2022-03-09 08:50:58 -0500

    Update 1st Move Case

--- src/state.ml
+++ src/state.ml
@@ -1,24 +1,42 @@
+open Board
+
 type t = {
   board : Board.t;
-  win_state : bool;
+  game_over : bool;
   victor : string;
   selected : (int * int) list;
 }
 (* Should be [""] for no victor, ["player 1"], and ["player 2"] *)
 
 let init_state (board : Board.t) : t =
-  { board; win_state = false; victor = ""; selected = [] }
+  { board; game_over = false; victor = ""; selected = [] }
 
 let get_board (state : t) : Board.t = state.board
-let game_over (state : t) : bool = state.win_state
+let game_over (state : t) : bool = state.game_over
 let get_victor (state : t) : string = state.victor
+
+let check_victor (state : t) : t =
+  if Board.num_pcs_of_pl state.board 1 = 0 then
+    { state with game_over = false; victor = "player 2" }
+  else if Board.num_pcs_of_pl state.board 2 = 0 then
+    { state with game_over = false; victor = "player 1" }
+  else state
 
 type move =
   | Continue of t
   | Legal of t
-  | Illegal
+  | Illegal of t
 
 let num_selected (state : t) : int = List.length state.selected
 
 let update (coord : int * int) (state : t) =
-  raise (Failure "Unimplemented: Board.pieces_of_player")
+  if
+    num_selected state = 0
+    &&
+    match coord with
+    | x, y -> (
+        match piece_of_xy state.board x y with
+        | Some piece -> true
+        | None -> false)
+  then Continue { state with selected = coord :: state.selected }
+  else Illegal state
