d574e0a154cf88148b25968a6acd0d5904bdb798
Cassidy Langenfeld <cml297@cornell.edu>
2022-03-08 19:13:40 -0500

    Update main.ml
    -Add method calls to keep from raising exceptions every time draw() is
    called

--- main/main.ml
+++ main/main.ml
@@ -1,6 +1,8 @@
 open Game
 (** [main ()] prompts for the game to play, then starts it. *)
-let main () = Game.Canvas.draw (Game.State.init_state Game.Board.init_board)
+let main () = Game.Canvas.draw (Game.State.init_state Game.Board.init_board);
+ Graphics.synchronize ();
+ Graphics.(ignore (wait_next_event [ Key_pressed ]));;
 
 (* Execute the game engine. *)
 let () = main ()
--- src/canvas.ml
+++ src/canvas.ml
@@ -1,11 +1,3 @@
-(**[tile_size] is a constant representing the x and y dimensions of a
-   tile. *)
-let tile_size = 40
-
-(**[pc_size] is a constant representing the horizontal and vertical
-   radius of a piece. *)
-let pc_size = 40
-
 (**[draw_piece pc x y] draws [pc] to the tile at [x] and [y].*)
 let draw_piece (pc : Board.piece) (x : int) (y : int) : unit =
   let open Graphics in
@@ -51,6 +43,4 @@ let draw st =
   let open Graphics in
   open_graph "";
   (*draw_board 0 (size_y () - (tile_size - 10)) 8 8 black 40;*)
-  draw_piece { player = 2; id = 1; is_royal = false } 50 50;
-  synchronize ();
-  ignore (wait_next_event [ Key_pressed ])
+  draw_piece { player = 2; id = 1; is_royal = false } 50 50
