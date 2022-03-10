11abf682b1273d451031ebede371017b31e3e277
Cassidy Langenfeld <cml297@cornell.edu>
2022-03-10 17:02:23 -0500

    -Fixed Canvas.draw_board
    -Fixed issue causing program to only draw one line of the board
    -The checkers board is now correctly drawn to the game canvas

--- src/canvas.ml
+++ src/canvas.ml
@@ -1,10 +1,13 @@
-let swap_color (c : Graphics.color) =
-  if c = Graphics.black then Graphics.red else Graphics.black
+let swap_color (c : Graphics.color) : Graphics.color =
+  if c = (Graphics.black : Graphics.color) then
+    (Graphics.red : Graphics.color)
+  else (Graphics.black : Graphics.color)
 
 (**[draw_piece pc x y] draws [pc] to the tile at [x] and [y].*)
 let draw_piece (b : Board.t) (pc : Board.piece) : unit =
   let open Graphics in
-  if pc.player = 1 then set_color red else set_color blue;
+  if pc.player = 1 then set_color (red : Graphics.color)
+  else set_color (blue : Graphics.color);
   match Board.xy_of_pc b pc with
   | Some (x, y) -> fill_ellipse x y 40 40
   | None -> ()
@@ -18,7 +21,7 @@ let rec draw_tile
     (tile_size : int)
     (color : Graphics.color) : unit =
   Graphics.moveto x y;
-  Graphics.set_color color;
+  Graphics.set_color (color : Graphics.color);
   Graphics.fill_rect x y tile_size tile_size
 
 (**[draw_row x y tile_size color cols] draws a row of [num] tiles
@@ -32,7 +35,9 @@ let rec draw_row
     (color : Graphics.color) : unit =
   draw_tile x y (Board.dim_x b) tile_size color;
   if i = 1 then ()
-  else draw_row (x + tile_size) y b (i - 1) tile_size (swap_color color)
+  else
+    draw_row (x + tile_size) y b (i - 1) tile_size
+      (swap_color (color : Graphics.color))
 
 (**[draw_board brd st] draws the board to the game canvas using the
    board configuration [bd].*)
@@ -47,13 +52,15 @@ let rec draw_board
   Graphics.moveto x y;
   if y_dim = 1 then ()
   else
-    draw_board x (y + tile_size) b (y_dim - 1) tile_size
-      (swap_color color)
+    draw_board x (y + tile_size) b (y_dim - 1)
+      (swap_color (color : Graphics.color))
+      tile_size
 
 let draw st =
-  let open Graphics in
-  open_graph "";
+  Graphics.open_graph "";
   let b = Board.init_board in
-  draw_board 50 50 b (Board.dim_y b) black 40
+  draw_board 50 50 b (Board.dim_y b)
+    (Graphics.black : Graphics.color)
+    40
 (*draw_tile 0 0 (Board.init_board |> Board.dim_x) 40 black*)
 (*draw_piece Board.init_board { player = 2; id = 1; is_royal = false*)
