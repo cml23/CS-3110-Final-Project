a6f6191856cc2ac27021232e2eb4859190c70b1b
Anirudh Sharma <as2844@cornell.edu>
2022-03-10 18:23:08 -0500

    Fix State.move type definition

--- src/state.mli
+++ src/state.mli
@@ -12,22 +12,25 @@ val init_state : Board.t -> t
 (** [init_state Board.t] creates an initial game state based on
     [Board.t]. *)
 
-(*val get_board : t -> Board.t (** [get_board t] returns the board
-  stored in state [t] for drawing. *)
+val get_board : t -> Board.t
+(** [get_board t] returns the board stored in state [t] for drawing. *)
 
-  val game_over : t -> bool (** [game_over t] returns whether a player
-  has won or not. *)
+val game_over : t -> bool
+(** [game_over t] returns whether a player has won or not. *)
 
-  val get_victor : t -> string (** [get_victor t] returns which player
-  has won. *)
+val get_victor : t -> string
+(** [get_victor t] returns which player has won. *)
 
-  (** The type representing the result of an attempted movement. *) type
-  move = | Continue of t | Legal of t | Illegal
+(** The type representing the result of an attempted movement. *)
+type move =
+  | Continue of t
+  | Legal of t
+  | Illegal of t
 
-  val update : int * int -> t -> move (** [update coord state] converts
-  a coordinate into a selection or piece movement on the board. [move]
-  is Continue or Legal if the first [coord] provided is contains a piece
-  and the second [coord] provided is an empty square satisfying certain
-  conditions. [move] is Illegal if the first [coord] provided does not
-  contain a piece or the second [coord] provided is an illegal checkers
-  move. *)*)
+val update : int * int -> t -> move
+(** [update coord state] converts a coordinate into a selection or piece
+    movement on the board. [move] is Continue or Legal if the first
+    [coord] provided is contains a piece and the second [coord] provided
+    is an empty square satisfying certain conditions. [move] is Illegal
+    if the first [coord] provided does not contain a piece or the second
+    [coord] provided is an illegal checkers move. *)
