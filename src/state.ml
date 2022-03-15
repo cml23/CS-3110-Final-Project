75e555c523a5a8e366c141050ef0ca155b13c662
hhf28 <hhf28@cornell.edu>
2022-03-15 19:34:14 -0400

    State Allows Piece Movement
    - Code does not compile, assumes not yet implemented functions in board.mli

--- src/state.ml
+++ src/state.ml
@@ -5,20 +5,26 @@ type t = {
   game_over : bool;
   victor : string;
   selected : (int * int) list;
-  legals : (int * int * piece option) list;
+  moves : (int * int) list;
+  caps : (int * int) list * piece list;
 }
 (* Should be [""] for no victor, ["player 1"], and ["player 2"] *)
 
 let init_state (board : Board.t) : t =
-  { board; game_over = false; victor = ""; selected = []; legals = [] }
+  {
+    board;
+    game_over = false;
+    victor = "";
+    selected = [];
+    moves = [];
+    caps = ([], []);
+  }
 
 let get_board (state : t) : Board.t = state.board
 let game_over (state : t) : bool = state.game_over
 let get_victor (state : t) : string = state.victor
-
-let get_legals (state : t) : (int * int * piece option) list =
-  state.legals
-
+let get_moves (state : t) : (int * int) list = state.moves
+let get_caps (state : t) : (int * int) list * piece list = state.caps
 let num_selected (state : t) : int = List.length state.selected
 let selected (state : t) : (int * int) list = state.selected
 
@@ -34,29 +40,45 @@ type move =
   | Legal of t
   | Illegal of t
 
-let pc_exist (state : t) (coord : int * int) : bool =
-  match coord with
-  | x, y -> (
-      match piece_of_xy state.board x y with
-      | Some piece -> true
-      | None -> false)
+let match_move (finish : int * int) (state : t) : bool =
+  List.exists (fun x -> x = finish) state.moves
 
-let nb_match nbf (start : int * int) (finish : int * int) : bool =
-  match start with
-  | x, y -> (
-      match nbf x y with
-      | Some final -> final = finish
-      | None -> false)
+let match_capture (finish : int * int) (state : t) : bool =
+  List.exists (fun x -> x = finish) (fst state.caps)
 
-let calculate_legals (state : t) (start : int * int) : (int * int) list
-    =
-  []
+(** [move_pc start finish state] takes two valid board coordinates, the
+    first one with a piece and the second without one.*)
+let move_pc (start : int * int) (finish : int * int) (state : t) :
+    Board.t =
+  del_pc
+    (add_pc state.board (piece_of_xy start) finish)
+    (fst start) (snd start)
 
+(* pos_moves *)
+(* pos_captures *)
 let update (coord : int * int) (state : t) =
-  if num_selected state = 0 && pc_exist state coord then
-    Continue { state with selected = coord :: state.selected }
+  if
+    num_selected state = 0
+    && pc_exists state.board (fst coord) (snd coord)
+  then
+    Continue
+      {
+        state with
+        selected = coord :: state.selected;
+        moves =
+          pos_moves (piece_of_xy state.board (fst coord) (snd coord));
+        caps =
+          pos_captures (piece_of_xy state.board (fst coord) (snd coord));
+      }
   else if num_selected state = 1 then
-    if nb_match (up_l state.board) (List.nth state.selected 0) coord
-    then Continue state
+    if match_move coord state then
+      Legal
+        {
+          state with
+          board = move_pc;
+          selected = [];
+          moves = [];
+          caps = ([], []);
+        }
     else Illegal state
   else Illegal state
