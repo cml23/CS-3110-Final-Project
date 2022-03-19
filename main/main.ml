b3e811a6fdb8e7eaecfa89d6608111b0d617a173
hhf28 <hhf28@cornell.edu>
2022-03-18 22:13:29 -0400

    Main Fixes

--- main/main.ml
+++ main/main.ml
@@ -30,7 +30,17 @@ let rec game_loop (game : t) (e : Graphics.status) : _ =
   else ();
   game_loop game event
 
-let main () = game_loop { state = State.init_state Board.init_board }
+let main () =
+  let game = { state = State.init_state Board.init_board } in
+  draw_state game;
+  let event = Graphics.wait_next_event [ Button_down; Key_pressed ] in
+  if event.button then
+    match Canvas.mouse_input event Game.Board.init_board with
+    | Some coord -> game_loop (process_move game coord) event
+    | None -> game_loop game event
+  else if event.keypressed then exit 0
+  else ();
+  game_loop game event
 
 (* Execute the game engine. *)
 let () = main ()
