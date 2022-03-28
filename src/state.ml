b876f7ca2fa471dabe234202835c139b706205e7
hhf28 <hhf28@cornell.edu>
2022-03-27 22:37:26 -0400

    Promotion and Multi Capture
    - Added promotion to move process pipeline
    - Added forced multi capture edge case functionality
    - Reworked functions to allow pipelining

--- src/state.ml
+++ src/state.ml
@@ -4,6 +4,12 @@ exception RemoveNoPiece
 exception NoPiece
 exception NoMove
 
+type multi_capture = {
+  present : bool;
+  piece : piece option;
+  caps : (int * int) list * piece list;
+}
+
 type t = {
   board : Board.t;
   game_over : bool;
@@ -12,7 +18,7 @@ type t = {
   moves : (int * int) list;
   caps : (int * int) list * piece list;
   player_turn : int;
-  mult_cap : bool;
+  mc : multi_capture;
 }
 (* Should be [""] for no victor, ["player 1"], and ["player 2"] *)
 
@@ -25,7 +31,7 @@ let init_state (board : Board.t) : t =
     moves = [];
     caps = ([], []);
     player_turn = 1;
-    mult_cap = false;
+    mc = { present = false; piece = None; caps = ([], []) };
   }
 
 let get_board (state : t) : Board.t = state.board
@@ -46,16 +52,16 @@ let check_victor (state : t) : t =
 (** [get_xy_of_pc state pc] is a wrapper function for xy_of_pc in board.
     Returns the coordinates of [pc]. Postcondition: raises [NoPiece] if
     [pc] does not exist in the board.*)
-let get_xy_of_pc (state : t) (pc : piece) : int * int =
-  match xy_of_pc state.board pc with
+let get_xy_of_pc (pc : piece) (board : Board.t) : int * int =
+  match xy_of_pc board pc with
   | Some c -> c
   | None -> raise NoPiece
 
 (** [get_pc_of_xy state coord] is a wrapper function for pc_of_xy in
     board. Returns the piece at [coord]. Postcondition: raises [NoPiece]
     if [coord] does not contain a piece.*)
-let get_pc_of_xy (state : t) (coord : int * int) : piece =
-  match piece_of_xy state.board (fst coord) (snd coord) with
+let get_pc_of_xy (coord : int * int) (board : Board.t) : piece =
+  match piece_of_xy board (fst coord) (snd coord) with
   | Some pc -> pc
   | None -> raise NoPiece
 
@@ -71,58 +77,52 @@ let rec get_idx
 
 (** [get_cap_pc coord state] returns the piece to be captured associated
     with [coord]. Precondition: [coord] is a valid second click.*)
-let get_cap_pc (coord : int * int) (state : t) : piece =
-  try get_idx (fst state.caps) coord 0 |> List.nth (snd state.caps)
+let get_cap_pc
+    (coord : int * int)
+    (caps : (int * int) list * piece list) : piece =
+  try get_idx (fst caps) coord 0 |> List.nth (snd caps)
   with _ -> raise NoMove
 
 (** [match_capture finish state] checks whether [finish] is is located
-    in state.moves.*)
-let match_move (finish : int * int) (state : t) : bool =
-  List.exists (fun x -> x = finish) state.moves
-
-(** [match_capture finish state] checks whether [finish] is is located
     in state.caps.*)
-let match_capture (finish : int * int) (state : t) : bool =
-  List.exists (fun x -> x = finish) (fst state.caps)
+let match_coord (finish : int * int) (coords : (int * int) list) : bool
+    =
+  List.exists (fun x -> x = finish) coords
 
 (** [move_pc start finish state] returns a board with a piece moved from
     [start] to [finish]. Precondition: [start] and [finish] are valid
     board coordinates and [start] contains a piece.*)
-let move_pc (start : int * int) (finish : int * int) (state : t) :
+let move_pc (start : int * int) (finish : int * int) (board : Board.t) :
     Board.t =
-  let pc = get_pc_of_xy state start in
-  let bd = add_pc state.board pc (fst finish) (snd finish) in
+  let pc = get_pc_of_xy start board in
+  let bd = add_pc board pc (fst finish) (snd finish) in
   del_pc bd (fst start) (snd start)
 
-(** [move_cap_pc start finish state] returns a board with a piece moved
-    from [start] to [finish] and with [rem_pc] removed. Precondition:
-    [start] and [finish] are valid board coordinates, [start] contains a
-    piece, and [rem_pc] exists in board.*)
-let move_cap_pc
-    (start : int * int)
-    (finish : int * int)
-    (rem_pc : piece)
-    (state : t) : Board.t =
-  let bd = move_pc start finish state in
-  let cd = get_xy_of_pc state rem_pc in
-  del_pc bd (fst cd) (snd cd)
+(** [move_cap_pc rem_pc board] returns a board with [rem_pc] removed.
+    Precondition:[rem_pc] exists in board.*)
+let cap_pc (rem_pc : piece) (board : Board.t) : Board.t =
+  let cd = get_xy_of_pc rem_pc board in
+  del_pc board (fst cd) (snd cd)
 
+(** [valid_reclick coord state] checks whether or not the [coord]
+    contains a piece in the board of [state] that matches the player
+    turn.*)
 let valid_reclick (coord : int * int) (state : t) : bool =
   pc_exists state.board (fst coord) (snd coord)
-  && (get_pc_of_xy state coord).player = state.player_turn
+  && (get_pc_of_xy coord state.board).player = state.player_turn
 
 (** [valid_fst_click coord state] checks whether or not [state] can
     store a coordinate (i.e. selected is empty) and whether the [coord]
-    contains a piece in the board of [state]*)
+    contains a piece in the board of [state].*)
 let valid_fst_click (coord : int * int) (state : t) : bool =
   unselected state && valid_reclick coord state
 
 (** [store_fst_click coord state] returns a state with selected
     containing [coord], moves containing legal moves from [coord], caps
-    containing legal caps from [coord]. Precondition: [coord] in board
-    contains a piece. *)
+    containing legal caps from [coord]. Precondition: [coord] is the
+    first click and contains a piece.*)
 let store_fst_click (coord : int * int) (state : t) : t =
-  let pc = get_pc_of_xy state coord in
+  let pc = get_pc_of_xy coord state.board in
   {
     state with
     selected = coord;
@@ -133,10 +133,13 @@ let store_fst_click (coord : int * int) (state : t) : t =
 (** [new_bd bd state] returns a state with board [bd] and the selected,
     moves, and caps fields cleared. Precondition: [bd] is the board
     after a legal move has occurred.*)
-let new_bd (state : t) (bd : Board.t) : t =
+let new_bd (state : t) (bd : Board.t) : t = { state with board = bd }
+
+(** [reset_st state] returns a state with the selected, moves, and caps
+    fields cleared. Precondition: a move has just occurred. *)
+let reset_st (state : t) : t =
   {
     state with
-    board = bd;
     selected = (-1, -1);
     moves = [];
     caps = ([], []);
@@ -145,18 +148,58 @@ let new_bd (state : t) (bd : Board.t) : t =
 
 (** [move_state state coord] returns a state with the selected piece
     moved to [coord] on the board. *)
-let move_state (state : t) (coord : int * int) : t =
-  let bd = move_pc state.selected coord state in
-  new_bd state bd
+let move_state (coord : int * int) (state : t) : t =
+  move_pc state.selected coord state.board |> new_bd state
 
 (** [move_cap_state state coord] returns a state with the selected piece
     moved to [coord] on the board and the intermittent piece captured. *)
-let move_cap_state (state : t) (coord : int * int) : t =
-  let pc = get_cap_pc coord state in
-  let bd = move_cap_pc state.selected coord pc state in
-  new_bd state bd
+let move_cap_state
+    (coord : int * int)
+    (caps : (int * int) list * piece list)
+    (state : t) : t =
+  let pc = get_cap_pc coord caps in
+  move_pc state.selected coord state.board |> cap_pc pc |> new_bd state
 
-(** [attempt_promote]*)
+(** [check_mc state coord] checks whether the piece that has moved to
+    [coord] can capture again without promotion and stores the
+    information in state. Precondition: [coord] contains a piece. *)
+let check_mc (coord : int * int) (state : t) : t =
+  let pc = get_pc_of_xy coord state.board in
+  let caps = poss_captures state.board pc in
+  if List.length (fst caps) > 0 then
+    {
+      state with
+      player_turn = pc.player;
+      mc = { present = true; piece = Some pc; caps };
+    }
+  else
+    {
+      state with
+      mc = { present = false; piece = None; caps = ([], []) };
+    }
+
+(** [pro_pc state coord] attempts to promote a piece that has moved to
+    [coord] and returns a new state accordingly. Should occur after
+    checking for multi capture to prevent post promotion captures.*)
+let pro_pc (coord : int * int) (state : t) : t =
+  let pc = get_pc_of_xy coord state.board in
+  if is_promotable state.board pc then
+    promote_pc state.board pc |> new_bd state
+  else state
+
+let legal_action (coord : int * int) (state : t) : bool =
+  match_coord coord state.moves || match_coord coord (fst state.caps)
+
+(** Precondition: Either a move or capture is possible. [coord] is valid
+    move or capture location of selected piece. *)
+let pipeline (move : bool) (mc : bool) (coord : int * int) (state : t) :
+    t =
+  if move then move_state coord state |> reset_st |> pro_pc coord
+  else
+    move_cap_state coord
+      (if mc then state.mc.caps else state.caps)
+      state
+    |> reset_st |> check_mc coord |> pro_pc coord
 
 (** [move] represents the type of state returned. Continue represents
     that the current player stays the same and requires new input. Legal
@@ -167,18 +210,29 @@ type move =
   | Legal of t
   | Illegal of t
 
+let match_mc_pc f (coord : int * int) (state : t) : bool =
+  f coord state
+  && Some (get_pc_of_xy coord state.board) = state.mc.piece
+
+let legal_mc (coord : int * int) (state : t) : move =
+  if match_mc_pc valid_fst_click coord state then
+    Continue (store_fst_click coord state)
+  else if
+    (not (unselected state)) && match_coord coord (fst state.mc.caps)
+  then Legal (pipeline false true coord state)
+  else if match_mc_pc valid_reclick coord state then
+    Continue (store_fst_click coord state)
+  else Illegal state
+
 (** [update coord state] returns a move depending on the validity of
     [coord] and the curren [state] *)
 let update (state : t) (coord : int * int) : move =
-  if valid_fst_click coord state then
+  if state.mc.present then legal_mc coord state
+  else if valid_fst_click coord state then
     Continue (store_fst_click coord state)
-  else
-    let leg_move = match_move coord state in
-    let leg_cap = match_capture coord state in
-    if (not (unselected state)) && (leg_move || leg_cap) then
-      if leg_move then Legal (move_state state coord)
-      else if leg_cap then Legal (move_cap_state state coord)
-      else Illegal state
-    else if valid_reclick coord state then
-      Continue (store_fst_click coord state)
-    else Illegal state
+  else if (not (unselected state)) && legal_action coord state then
+    let legal_move = match_coord coord state.moves in
+    Legal (pipeline legal_move false coord state)
+  else if valid_reclick coord state then
+    Continue (store_fst_click coord state)
+  else Illegal state
