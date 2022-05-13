7bc355836a479a54301788f3716e7a87e7a490c6
hhf28 <hhf28@cornell.edu>
2022-05-13 13:30:09 -0400

    Forfeit and Refactoring

--- main/main.ml
+++ main/main.ml
@@ -11,14 +11,17 @@ type t = {
   use_ai : bool;
 }
 
-(* [default_game]*)
 let file_format = ".json"
+(* [fileformat] is the file type for saved files. *)
 
 let default_game = "default_layout"
+(* [default_game] is the filename for the default board. *)
 
 let data_dir_prefix = "data" ^ Filename.dir_sep
+(* [data_dir_prefix] is the filelcations for saved files. *)
 
 let game_name = ref (data_dir_prefix ^ "default_layout" ^ file_format)
+(* [game_name] is the current file being used to play the game. *)
 
 (*=========LOADERS=========*)
 
@@ -27,22 +30,16 @@ let game_name = ref (data_dir_prefix ^ "default_layout" ^ file_format)
 (* The json representation of the game stores the current board, the
    current player turn, and the multi-game score tally. *)
 
-let to_json (g : t) : Yojson.Basic.t =
-  `Assoc
-    [
-      ("board", Board.to_json (State.get_board g.state));
-      ("turn", `Int (State.get_player g.state));
-      ("player 1 score", `Int g.p1_sc);
-      ("player 2 score", `Int g.p2_sc);
-      ("preset", `Int g.preset);
-    ]
-
+(** [from_json_state json] only returns the [game.state] constructed
+    from [json] for the purposes of restarting a game. *)
 let from_json_state json =
   let open Yojson.Basic.Util in
   State.init_state
     (json |> member "turn" |> to_int)
     (Board.from_json (json |> member "board"))
 
+(** [from_json useai1 json] creates a new game state based on [json] and
+    whether [useai1] is true or not.*)
 let from_json (use_ai1 : bool) (json : Yojson.Basic.t) : t =
   let open Yojson.Basic.Util in
   let st = from_json_state json in
@@ -55,6 +52,19 @@ let from_json (use_ai1 : bool) (json : Yojson.Basic.t) : t =
     use_ai = use_ai1;
   }
 
+(** [to_json] converts a game into a save file with board, turn, player
+    score, and preset information svaed. *)
+let to_json (g : t) : Yojson.Basic.t =
+  `Assoc
+    [
+      ("board", Board.to_json (State.get_board g.state));
+      ("turn", `Int (State.get_player g.state));
+      ("player 1 score", `Int g.p1_sc);
+      ("player 2 score", `Int g.p2_sc);
+      ("preset", `Int g.preset);
+    ]
+
+(** [save_game g name] converts a game into a save file. *)
 let save_game (g : t) (name : string) : unit =
   Yojson.Basic.to_file
     (data_dir_prefix ^ name ^ file_format)
@@ -64,22 +74,33 @@ let save_game (g : t) (name : string) : unit =
 
 (*=========GAME MANIPULATION FUNCTIONS=========*)
 
+(** [is_p1_win game] returns whether player 1 has won or not.
+    Precondition: the game is over. *)
 let is_p1_win (game : t) : bool =
   if not (game.state |> State.game_over) then
     failwith "Called when game is not over"
   else game.state |> State.get_vc = 1
 
+(** [change_st chg v game] takes a function [chg] that modifies
+    [game.state] and applies it to [game] with value [v]. *)
 let change_st (chg : 'a -> State.t -> State.turn) (v : 'a) (game : t) :
     t =
   let nt = chg v game.state in
   { game with state = State.get_state nt; turn = nt }
 
+(** [process_mv] is a wrapper function for the [update] function in
+    [State] to pass a selected square to state. *)
 let process_mv (coord : int * int) (game : t) : t =
   change_st State.update coord game
 
+(** [undo_mv undo game] is a wrapper function for the [urdo] function in
+    [State] to return the game state to the previous move. *)
 let urdo_mv (undo : bool) (game : t) : t =
   change_st State.urdo undo game
 
+(** [change_sc game] updates the scores by one for player 1 if [p1_win]
+    is [true] and for player 2 otherwise. Precondition: the game is
+    over. *)
 let change_sc (p1_win : bool) (game : t) : t =
   let new_game =
     {
@@ -92,36 +113,61 @@ let change_sc (p1_win : bool) (game : t) : t =
   print_endline (string_of_int new_game.p2_sc);
   new_game
 
+(** [restart_gm game] reloads the game based on the file name selected. *)
 let restart_gm (game : t) : t =
   let new_state =
     !game_name |> Yojson.Basic.from_file |> from_json_state
   in
   { game with state = new_state; turn = Legal new_state }
 
+(** [change_preset game] changes the preset number to next one available
+    for screen recoloring. *)
 let change_preset (game : t) : t =
   { game with preset = Game.Canvas.swap_preset game.preset }
 
+(** [forfeit game] restarts the board so the player who did not forfeit
+    gains one point in score. *)
+let forfeit (game : t) : t =
+  let p1w =
+    if game.state |> State.get_player = 1 then false else true
+  in
+  game |> change_sc p1w |> restart_gm
+
 (*=========DRAW FUNCTIONS========*)
+
+(** [draw_st game] calls the canvas [draw] function with preset
+    information. *)
 let draw_st (game : t) : _ = game.state |> Game.Canvas.draw game.preset
 
+(** [draw_sc game] calls the canvas [draw_new_game] function with preset
+    and score information. *)
+let draw_sc (game : t) : _ =
+  Canvas.draw_new_game game.preset game.p1_sc game.p2_sc game.state
+
+(** [highlight e game] highlights the specific square that has been
+    selected by calling the canvas [highlight] function. *)
 let highlight (e : Graphics.status) (game : t) : _ =
   game.state |> State.get_board |> Canvas.highlight e
 
 (*=========GAME LOOP=========*)
-(* [glref event game] stores [game_loop] as a *)
+(* [glref event game] stores [game_loop] as a reference to allow access
+   throughout the program. *)
 let gl_ref = ref (fun a b -> ())
 
+(** [check_win glref e game] checks for two player games whether one
+    player can move or not and updates the score accordingly. *)
 let check_win glref (e : Graphics.status) (game : t) : _ =
   if game.state |> State.game_over then (
     Unix.sleepf 1.0;
     let new_sc_game =
       game |> change_sc (game |> is_p1_win) |> restart_gm
     in
-    Canvas.draw_new_game new_sc_game.preset new_sc_game.p1_sc
-      new_sc_game.p2_sc new_sc_game.state;
+    draw_sc new_sc_game;
     !gl_ref e new_sc_game)
-  else ()
 
+(** [check_ai glref e game] makes a move with the AI if the player has
+    opted for PvE. Also checks for the win condition when the AI cannot
+    make any new moves.*)
 let check_ai glref (e : Graphics.status) (game : t) : _ =
   if game.use_ai && State.get_player game.state = 2 then
     try
@@ -132,10 +178,17 @@ let check_ai glref (e : Graphics.status) (game : t) : _ =
       in
       !glref e gm
     with
-    | Game.Ai.NoMove -> game |> change_sc true |> restart_gm |> !glref e
+    | Game.Ai.NoMove ->
+        Unix.sleepf 1.0;
+        let new_sc_game = game |> change_sc true |> restart_gm in
+        draw_sc new_sc_game;
+        !glref e new_sc_game
     | current_game -> ()
 
-(* [event_handler glref game]*)
+(* [event_handler glref game] performs operations on the game state
+   depending on the input from the user. "z" undos a move, "x" redos a
+   move, "r" restarts the game, keeping the score, "c" changes the color
+   of the game, "s" saves the game, "q" quits the game.*)
 let rec event_handler glref (game : t) : _ =
   let event = Graphics.wait_next_event [ Button_down; Key_pressed ] in
   let gle = !glref event in
@@ -145,6 +198,7 @@ let rec event_handler glref (game : t) : _ =
     | None -> game |> gle
   else if event.key = 'z' then game |> urdo_mv true |> gle
   else if event.key = 'x' then game |> urdo_mv false |> gle
+  else if event.key = 'f' then game |> forfeit |> gle
   else if event.key = 'r' then game |> restart_gm |> gle
   else if event.key = 'c' then game |> change_preset |> gle
   else if event.key = 's' then (
@@ -154,7 +208,10 @@ let rec event_handler glref (game : t) : _ =
   else game |> gle;
   ()
 
-(* [game_loop e game]*)
+(* [game_loop e game] is the main body of the game, which draws the
+   board, checks the win conditions, makes ai moves, highlights the
+   selected square. If nothing special happens, called
+   [event_handler]. *)
 let rec game_loop (e : Graphics.status) (game : t) : _ =
   (* if State.game_over game.state then let game = (change_sc game); *)
   draw_st game;
@@ -163,23 +220,25 @@ let rec game_loop (e : Graphics.status) (game : t) : _ =
   if e.button then highlight e game else ();
   event_handler gl_ref game
 
-(* [start_game game]*)
+(* [start_game game] takes in a [game], sets the game loop reference,
+   and begins the main game loop. *)
 let start_game game =
   Canvas.draw_new_game game.preset game.p1_sc game.p2_sc game.state;
   gl_ref := game_loop;
   event_handler gl_ref game;
   ()
 
-(* [gf_ref] stores [game_loop] as a *)
-(* let gf_ref = ref (fun a -> ()) *)
-
+(* [get_file loader] takes a filename and appends to necessary
+   information to for parsing. *)
 let rec get_file loader =
   match read_line () with
   | exception End_of_file -> ()
   | "default" -> loader (data_dir_prefix ^ default_game ^ file_format)
   | file_name -> loader (data_dir_prefix ^ file_name ^ file_format)
 
-(* [load_game f]*)
+(* [load_game use_ai f] constructs a game of type t from filename [f]
+   and sets whether to use the ai based on [use_ai] and starts the game
+   with said state. *)
 let rec load_game (use_ai : bool) (f : string) =
   try
     game_name := f;
@@ -226,7 +285,9 @@ let get_player_name player name =
   name := get_name ();
   print_endline ""
 
-let rec get_yes_no (loader : unit) : bool =
+(** [get_yes_no] is a helper function for [get_ai]. Returns [true] if
+    the player wants an ai by typing ["1"]. *)
+let rec get_yes_no () : bool =
   match read_line () with
   | "1" -> true
   | "2" -> false
@@ -234,6 +295,8 @@ let rec get_yes_no (loader : unit) : bool =
       print_endline "Please choose 1 or 2. \n";
       get_yes_no ()
 
+(** [get_ai] asks the player in the terminal whether they want to have
+    an ai or not. Returns [true] if the player wants an ai. *)
 let get_ai () : bool =
   print_endline "Do you have 1 or 2 players?";
   print_string "> ";
@@ -241,7 +304,8 @@ let get_ai () : bool =
   print_endline "";
   use_ai
 
-(** [main] starts a game based on .*)
+(** [main] starts a game, and asks the player a series of questions to
+    set up the game.*)
 let main () =
   ANSITerminal.print_string [ ANSITerminal.red ]
     "\n\nOCaml Checkers Initialized\n";
