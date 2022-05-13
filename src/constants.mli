db082046b44e0a1621b0e1e6aea42daaac9b6a0e
Cassidy Langenfeld <cml297@cornell.edu>
2022-05-12 22:32:51 -0400

    Updated constants.mli
    -Changed start_x and start_y documentation to better reflect their usage
    in this stage of the project

--- src/constants.mli
+++ src/constants.mli
@@ -5,9 +5,9 @@ val pc_size : int
 (**[pc_size] is the radius of the piece.*)
 
 val start_x : int
-(**[start_x] is a constant representing the starting x position for
-   drawing the board.*)
+(**[start_x] is a constant used in resizing the game window for
+   different boards.*)
 
 val start_y : int
-(**[start_y] is a constant representing the starting y position for
-   drawing the board.*)
+(**[start_y] is a constant used in resizing the game window for
+   different boards.*)
